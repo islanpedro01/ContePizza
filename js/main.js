91bdfbd69d908a315e1766e0055ddd550d223cea
islanpedro01 <islan.figueiredo@academico.ifpb.edu.br>
2025-03-14 16:55:30 -0300

    Syle and logic almost completed

--- js/main.js
+++ js/main.js
@@ -2,8 +2,37 @@ const nameInput = document.getElementById("nome")
 const nameInputButton = nameInput.button
 const contadoresContainer = document.getElementById("contadores")
 
-function addContador(){
-  contadoresContainer.innerHTML += `<div class ="mt-10 flex flex-row justify-center h-12"> <label class="font-semibold self-center text-center w-32 overflow-hidden">`+ nameInput.value +`</label> <div class="relative ml-2 h-full"> <button class="absolute"><i class="fa-solid fa-circle-minus"></i></button> <input type="text" value="0" class="block h-full w-24 text-center text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50" readonly/> <button class="absolute end-5"><i class="fa-solid fa-circle-plus"></i></button> </div> </div>`
+var i = 0;
 
 
+function addContador() {
+
+i += 1;
+console.log(i)
+
+
+
+  contadoresContainer.innerHTML += `<div class="mt-10 flex flex-row justify-center h-12"> 
+                <div class="ml-2 h-full flex "> 
+                    <input type="text" value="`+ nameInput.value + `"class="font-semibold  text-[1rem] self-center text-center w-32 overflow-hidden block h-full w-24 text-center text-sm text-gray-900 border border-gray-400 active:border-none bg-gray-50"readonly/>
+                           <span class="relative overflow-hidden"> 
+                            <button onclick="removerFatia()" class="botao`+i+` absolute start-0 -top-1.5 text-xl bg-blue-600 text-white rounded-md py-4 px-2.5 cursor-pointer">
+                                <i class="fa-solid fa-minus pointer-events-none"></i>
+                            </button>
+                            <input type="text" value="0"
+                        class="botao`+i+` block h-full w-42 text-center text-2xl text-gray-900 border border-gray-400 rounded-lg bg-gray-50"
+                        readonly /> <button onclick="addfatia()" class="botao`+i+` absolute end-0 -top-1.5 text-xl bg-blue-600 text-white rounded-md py-4 px-2.5 cursor-pointer"><i class="fa-solid fa-plus pointer-events-none"></i></button> </span></div>`
+
+}
+
+function addfatia(){
+  event.target.previousElementSibling.value++
+
+}
+
+function removerFatia(){
+  if (event.target.nextElementSibling.value > 0){
+    event.target.nextElementSibling.value--
+  }
+  
 }
